149668d158ae970624c5ae36bda96c6e70bde40f
murillo dev72 <148728763+murillodev72@users.noreply.github.com>
2023-10-29 13:14:36 -0300

    Add files via upload

---
+++ code.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace CalculadoraBasica{
+class Calculadora{
+static void Main(String[] args){
+
+double num1, num2, resultado;
+string tmp; //tmp: temporário
+
+Console.WriteLine("digite um número: ");
+tmp = Console.ReadLine();
+num1 = int.Parse(tmp);
+
+Console.WriteLine("digite outro número: ");
+tmp = Console.ReadLine();
+num2 = int.Parse(tmp);
+
+resultado = num1+num2;
+Console.WriteLine("soma: "+ resultado);
+
+resultado = num1-num2;
+Console.WriteLine("subtração: "+ resultado);
+
+resultado = num1*num2;
+Console.WriteLine("multiplicação: "+ resultado);
+
+resultado = num1/num2;
+Console.WriteLine("divisão: "+ resultado);
+}
+}
+} 
